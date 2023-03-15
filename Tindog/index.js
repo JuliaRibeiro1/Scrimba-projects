ae7f32ea513a876336254e2cf76a30358f114ddc
unknown <hijulia259@gmail.com>
2023-03-15 11:18:11 -0300

    utils.js adicionado e localstorage funcionando

--- Tindog/index.js
+++ Tindog/index.js
@@ -1,26 +1,39 @@
-import {dogs,dogs2} from "./data.js"
+import {dogsArr} from "./data.js"
+import {liked,swiped,endProfiles} from "./utils.js"
+//let dogsProfiles = dogsArr[0]
 
+//localStorage.clear()
+localStorage.setItem("isFirstTime",true)
+let dogsProfiles = dogsArr
+console.log(dogsProfiles)
+if(localStorage.getItem("isFirstTime") == true) {
+    console.log("Oi")
+    localStorage.setItem("doggos",JSON.stringify(dogsArr[0]))
+    localStorage.setItem("isFirstTime",false)
+    
+}
 
-console.log(dogs2)
-let dogsProfiles = dogs
 //localStorage.setItem("doggos",JSON.stringify(dogsProfiles))
+
 let doggos = JSON.parse(localStorage.getItem("doggos"))
+
 let currentProfile = document.createElement("div")
 let profilesSection = document.querySelector(".profiles-section")
 let profilesHtml = document.querySelector(".profiles")
 let i = 0
 
-renderProfile(JSON.parse(localStorage.getItem("doggos")))
+
+doggos.length > 0 ? renderProfile(doggos[0]) : profilesSection.innerHTML = endProfiles()
 
 //localStorage.setItem("length", JSON.stringify(dogsProfiles.length))
 let length = window.localStorage.getItem("length")
 let btnDisabled = true
-console.log(length)
+console.log(doggos[0])
 document.querySelectorAll(".btns-container button").forEach((button) => button.addEventListener("click",() => {
   i = 0
   if(btnDisabled == true) {
     if(button.className == "like-btn") { 
-            liked(dogsProfiles)
+            liked(doggos[0])
             profilesHtml.innerHTML += `<span class="badge-span"><img src="images/badge-like.png"></span>`
     }
      else if(button.className == "pass-btn") {
@@ -29,30 +42,31 @@ document.querySelectorAll(".btns-container button").forEach((button) => button.a
     }
     btnDisabled = false
     //dogsProfiles.length
-  if(doggos.length > 1) {
-    console.log(doggos)
+  if(doggos[0].length > 1) {
         setTimeout(() => {
-            //dogsProfiles.shift()
+          
+            doggos[0].shift()
             localStorage.setItem("doggos",JSON.stringify(doggos))
-            doggos.shift()
-            
-        ///   doggos.shift()
-            swiped(doggos)
-            renderProfile(doggos)
+            swiped(doggos[0])
+            renderProfile(doggos[0])
             btnDisabled=true 
-           
-            
-           
-        },900)
-    
+ 
+        },900) 
 }
 else {
     setTimeout(() => {
       //  doggos.shift()
+     // if(doggos.length > 0) {
         profilesSection.innerHTML = endProfiles()
-        localStorage.setItem("doggos",JSON.stringify(dogs2))
-        renderProfile(doggos)
-
+        doggos.shift()
+        
+         localStorage.setItem("doggos",JSON.stringify(doggos))
+         swiped(doggos[0])
+     /*    renderProfile(doggos[0]) 
+      }
+      else {
+        profilesSection.innerHTML = endProfiles()
+      }*/
        
     },1000)
 
@@ -67,7 +81,7 @@ function getCurrentImg(profile) {
 }
 
 function renderProfile(profile) {
-    console.log(profile)
+   
     const {avatar,name,bio,age} = profile[0]
   let imgsDots = ""
     avatar.map((dot,index) => {
@@ -78,7 +92,6 @@ function renderProfile(profile) {
             imgsDots += `<button class="dot"></button>`
         }
     })
-
     currentProfile.innerHTML = `
     ${avatar.length > 1? `<span class="dots-container ">${imgsDots}</span>`:""}
     <img class="current-img" src=${avatar[0]}></img>
@@ -90,7 +103,6 @@ function renderProfile(profile) {
     profilesHtml.append(currentProfile)
 
     return profilesHtml.innerHTML = currentProfile.innerHTML
-  
 }
 
 let parent;
@@ -116,23 +128,3 @@ if(e.target.className == "current-img") {
     getCurrentImg(doggos)
 }
 })
-
-function liked(profile) {
-    return profile[0].hasBeenLiked = true
-}
-function swiped(profile) {
-    return profile[0].hasBeenSwiped = true
-}
-function endProfiles() {
-    let endText = ""
-    endText = `
-    <div class="end-message-container">
-    <svg class="heart" viewBox="0 0 32 29.6">
-    <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
-      c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
-    </svg> 
-        <p class="endMessage">Oops, that's it for now, come back later for new more matches!</p>
-    </div>
-    `
-    return endText
-}
