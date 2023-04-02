c4f98660b9e74b63f16f02526d6f7261b28640de
unknown <hijulia259@gmail.com>
2023-04-02 15:46:25 -0300

    ravel journal responsivo

--- Travel Journal/travel journal/src/components/Note.jsx
+++ Travel Journal/travel journal/src/components/Note.jsx
@@ -5,20 +5,22 @@ export default function Note(props) {
     const {title,location, googleMapsUrl,startDate,endDate,description,imageUrl} = props
     return (
         <article>
-            <section className="not-img-container">
+            <section className="note-img-container">
             <img className="note-img" src={imageUrl}></img>
             </section>
+            <section className="note-container">
             <section className="note-header">
                 <header>
                     <div className="note-location"><img src={path}/>{location}</div>
-                    <div><a src={googleMapsUrl}>View on Google Maps</a></div>
+                    <div><a href={googleMapsUrl}>View on Google Maps</a></div>
                 </header>
                 
             </section>
-            <section className="note-container">
+            <section className="note">
                 <h1>{title}</h1>
                 <div className="note-date"><span>{startDate}</span>-<span>{endDate}</span></div>
                 <p>{description}</p>
+                </section>
                 </section>
         </article>
     )
